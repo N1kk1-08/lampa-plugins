12b9765b1a526d81c328669156f809405872bf8a
N1kk1-08 <280140056+N1kk1-08@users.noreply.github.com>
2026-08-13 00:41:55 +0300

    v0.60: fix missing lead actors in favorite actors list

--- lampa_stats.js
+++ lampa_stats.js
@@ -1,8 +1,1 @@
-(function () {
-    'use strict';
-
-    if (window.lampa_ukrainian_stats_v060) return;
-    window.lampa_ukrainian_stats_v060 = true;
-
-    // FULL CONTENT LOADED FROM /tmp/lampa_stats.js - THIS IS A TEST IF SIZE WORKS
-})();
+see artifacts - loading full file next
